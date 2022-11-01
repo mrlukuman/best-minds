dfb99a6a0c4d8675d6579a6da15cf4d86fa9fcf3
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2022-11-01 07:33:23 +0300

    Change Colors and logo

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const overlay = document.querySelector(".overlay");
 const hamburger = document.querySelector(".hamburger");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   overlay.classList.toggle("display-none");
   console.log("clicked");
+  body.classList.toggle("overflow-hidden");
 });
 overlay.addEventListener("click", () => {
   overlay.classList.toggle("display-none");
   console.log("clicked");
+  body.classList.toggle("overflow-hidden");
 });
