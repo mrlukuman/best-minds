74a249dafe2b20ed226687cdb6dbb1524a9d0042
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2022-11-01 07:55:37 +0300

    Add over-flow: hidden to html instead of body to fix mobile overflow when modal is open

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const overlay = document.querySelector(".overlay");
 const hamburger = document.querySelector(".hamburger");
-const body = document.querySelector("body");
+const html = document.querySelector("html");
 
 hamburger.addEventListener("click", () => {
   overlay.classList.toggle("display-none");
   console.log("clicked");
-  body.classList.toggle("overflow-hidden");
+  html.classList.toggle("overflow-hidden");
 });
 overlay.addEventListener("click", () => {
   overlay.classList.toggle("display-none");
